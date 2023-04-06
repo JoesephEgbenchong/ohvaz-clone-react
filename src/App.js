2bea8caa5615ec0be70beae8e219a0ba9101bcc9
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2023-04-06 03:55:24 +0100

    Install router dom and create pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Locations from './pages/Locations';
+import Press from './pages/Press';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import NoPage from './pages/NoPage';
 
 
 function App() {
   return (
-    <>
-      <h1  className="text-xl text-center mt-6 font-Poppins text-textBlack font-semibold">Let's get the Ohvaz website started</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/press" element={<Press />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
