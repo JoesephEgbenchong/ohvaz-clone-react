01f10ef46329d1d7ee986460b7f8ea2141a31dda
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2023-04-13 21:20:58 +0100

    Corrected the mobile version navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,15 +33,20 @@ export default function Header() {
                     {/* Toggle button for mobile navbar */}
                     <button type='button' className='block text-textBlack hover:text-primary focus:text-primary focus:outline-none'
                             onClick={() => setShowMenu(!showMenu)}>
-                        <svg viewBox='0 0 20 20' className='w-6 h-6 fill-current'>
-                            <title>Menu</title>
-                            <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'/>
-                        </svg>
+                            {showMenu ? (
+                            <svg className='w-6 h-6' fill='none' stroke='#000' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
+                            </svg>
+                            ) : (
+                            <svg className='w-6 h-6' fill='none' stroke='#000' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16M4 18h16' />
+                            </svg>
+                            )}
                     </button>
             </div>
              {/* Mobile navbar that displays rest of the menu items */}
              {showMenu && (
-                <div className='absolute top-24 left-0 right-0 bg-white md:hidden'>
+                <div className='absolute top-24 left-0 right-0 z-50 bg-white md:hidden'>
                     <ul className='flex flex-col space-y-2 justify-center items-center mb-1'>
                         <li className={` text-xs font-Poppins uppercase font-semibold hover:text-primary
                             transition duration-150 ease-in-out 
